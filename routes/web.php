fef808d4dcbb00953781cc2a143a556aaf39f237
Jeremy Stephens <jrseducate@gmail.com>
2023-09-27 12:21:40 -0400

    jrseducate: Inverted the V in UV, the coordinate system in THREE is bottom-up while the coordinates in Canvas is top-down

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,10 @@ Route::get('/', function () {
     ]);
 });
 
+Route::get('/phpinfo', function () {
+    phpinfo();
+});
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
